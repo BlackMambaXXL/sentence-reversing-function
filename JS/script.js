1f233d4592f9db9386c40ffceb19ee89fce4b9ea
Niyaz <khairutdinov96@gmail.com>
2023-02-04 19:43:32 +0300

    done

--- JS/script.js
+++ JS/script.js
@@ -1,17 +1,21 @@
 const someString = 'This is some strange string';
 
 function reverse(str) {
-    let arr = str.split(" ");
-    arr.reverse();
-    let innerArr = [];
-    for (let i = 0; i < arr.length; i++) {
-        innerArr[i] = arr[i].split("");
-        innerArr[i].reverse();
-    }
+    if (typeof(str) === "string") {
+        let arr = str.split(" ");
+        arr.reverse();
+        let innerArr = [];
+        for (let i = 0; i < arr.length; i++) {
+            innerArr[i] = arr[i].split("");
+            innerArr[i].reverse();
+        }
 
-    let reversedArr = innerArr.join(" ");
-    reversedArr = reversedArr.split(",").join("");
-    console.log(reversedArr);
+        let reversedArr = innerArr.join(" ");
+        reversedArr = reversedArr.split(",").join("");
+        console.log(reversedArr);
+    } else {
+        console.log("Ошибка.");
+    }
 }
 
 reverse(someString);
