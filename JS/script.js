3e46a2b03136da528f8db084dd2856370642d863
Niyaz <khairutdinov96@gmail.com>
2023-02-04 19:48:45 +0300

    the answer

--- JS/script.js
+++ JS/script.js
@@ -2,17 +2,7 @@ const someString = 'This is some strange string';
 
 function reverse(str) {
     if (typeof(str) === "string") {
-        let arr = str.split(" ");
-        arr.reverse();
-        let innerArr = [];
-        for (let i = 0; i < arr.length; i++) {
-            innerArr[i] = arr[i].split("");
-            innerArr[i].reverse();
-        }
-
-        let reversedArr = innerArr.join(" ");
-        reversedArr = reversedArr.split(",").join("");
-        console.log(reversedArr);
+        console.log (str.split('').reverse().join(''));
     } else {
         console.log("Ошибка.");
     }
